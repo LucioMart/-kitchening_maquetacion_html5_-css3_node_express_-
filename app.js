0796a7174d66ae4c9955b5e48555fd689887228f
LucioMart <luchoweb15@gmail.com>
2021-08-31 10:24:32 -0300

    Reestructurando el proyecto para aplicar el modelo mvc, y utilizar router y controller.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require('express')
 const app = express()
-const path = require('path')
+const path = require('path');
 const port = 3030
 
+const indexRouter = require('./routes/index')
+ 
 app.use(express.static('public'))
 
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'home.html'))
-})
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use('/', indexRouter)
 
 app.listen(port, () => console.log('Server running in port '+ port))
