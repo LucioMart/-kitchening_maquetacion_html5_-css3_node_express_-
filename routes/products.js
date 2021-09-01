748a88201e4f929fea31fff64f9d3d17471b9036
LucioMart <luchoweb15@gmail.com>
2021-09-01 18:15:17 -0300

    Realizado las rutas a los metodos del resto del crud, en products.

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {add, detail} = require('../controller/productsController')
+const {add, detail, search, store, edit, update, destroy} = require('../controller/productsController')
 
 router.get('/add', add)
 router.get('/detail/:id', detail)
-router.get('/search')
+router.get('/search', search)
+router.post('/add', store)
+router.get('/edit/:id', edit)
+router.put('/update/:id')
+router.delete('/delete/:id')
 
 
 module.exports = router
