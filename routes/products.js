42aad9dd065e071f2f33ed6e43434f6bf24b7a94
LucioMart <luchoweb15@gmail.com>
2021-09-02 12:58:44 -0300

    agregado la validación a la ruta de agregar productos.

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const {add, detail, search, store, edit, update, destroy} = require('../controller/productsController')
+const productsAddValidator = require('../validations/productsAddValidator')
 
 router.get('/add', add)
 router.get('/detail/:id', detail)
 router.get('/search', search)
-router.post('/add', store)
+router.post('/add',productsAddValidator, store)
 router.get('/edit/:id', edit)
-router.put('/update/:id')
-router.delete('/delete/:id')
+router.put('/update/:id', update)
+router.delete('/delete/:id', destroy)
 
 
 module.exports = router
