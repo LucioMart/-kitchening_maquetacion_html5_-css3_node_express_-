7c08dcf95767ab58c27ca5db2a8b480c6b80b18b
LucioMart <luchoweb15@gmail.com>
2021-09-03 21:32:06 -0300

    Require module method-override en el app.

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const port = 3030
+const mehodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const productsRouter = require('./routes/products')
  
 app.use(express.static('public'))
+
+app.use(mehodOverride('_method'))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
