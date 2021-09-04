b41f88feb4fd0a309333c34076825dc79ff6967f
LucioMart <luchoweb15@gmail.com>
2021-09-04 20:48:01 -0300

    Instalando los modulos session, cookie y  bcryptjs. Requiriendo los mudulos en el entrypoint.

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const app = express()
 const path = require('path');
 const port = 3030
 const mehodOverride = require('method-override')
+const session = require('express-session')
 
 const indexRouter = require('./routes/index')
-const productsRouter = require('./routes/products')
+const productsRouter = require('./routes/products');
+const cookieParser = require('cookie-parser');
  
 app.use(express.static('public'))
 
 app.use(mehodOverride('_method'))
+app.use(session({
+    secret : 'mi secreto',
+    saveUninitialized : true,
+    resave : false
+}))
+
+app.use(cookieParser())
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
