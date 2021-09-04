a536673f55a5279ea1846faa54bdd5c4de2a1079
LucioMart <luchoweb15@gmail.com>
2021-09-03 22:59:42 -0300

    Agregar images validation y product validation a update put..

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const {add, detail, search, store, edit, update, destroy} = require('../controller/productsController')
-const productsAddValidator = require('../validations/productsAddValidator')
+
+const productsValidator = require('../validations/productsValidator')
+const productsImageStore = require('../middlewares/productImageStore')
+
 
 router.get('/add', add)
 router.get('/detail/:id', detail)
 router.get('/search', search)
-router.post('/add',productsAddValidator, store)
+router.post('/add',productsValidator, store)
 router.get('/edit/:id', edit)
-router.put('/update/:id', update)
+router.put('/update/:id',productsImageStore.array('image'), productsValidator, update)
 router.delete('/delete/:id', destroy)
 
 
