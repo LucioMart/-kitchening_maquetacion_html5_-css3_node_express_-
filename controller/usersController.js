8dac6846600f03397dce7edabf59d13faf8f6e1d
LucioMart <luchoweb15@gmail.com>
2021-09-05 09:49:42 -0300

    Terminado  de la logica del proceso de registro, en el usersController.Validaciones y creación del usuario.

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,4 +1,9 @@
 const { validationResult } = require("express-validator")
+const bcript = require('bcryptjs')
+const users = require('../data/users.json')
+const fs = require('fs')
+const path = require('path')
+
 
 module.exports = {
     register: (req, res) => {
@@ -9,6 +14,33 @@ module.exports = {
 
         if(errors.isEmpty()) {
             const { name, email, pass } = req.body
+
+            let user = {
+                id : users.length > 0 ? users[users.length - 1].id + 1 : 1,
+                name : name.trim(),
+                email : email.trim(),
+                pass : bcript.hashSync(pass.trim(), 10),
+                rol : 'user',
+                avatar : 'avatar_default.png'
+            }
+
+            users.push(user)
+            fs.writeFileSync(path.join(__dirname, '..', 'data', 'users.json'), JSON.stringify(users, null, 2), 'utf-8')
+
+            req.session.userLogin = {
+                id: user.id,
+                name: user.name,
+                avatar: user.avatar,
+                rol: user.rol
+            }
+
+            return res.redirect('/')
+
+        } else {
+            return res.render('register', {
+                old: req.body,
+                errors: errors.mapped()
+            })
         }
     },
     login: (req, res) => {
